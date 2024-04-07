b8157c838cc8362317c4575c2d6de79c971ab974
linzhiji <linzhiji@gmail.com>
2024-04-07 15:49:27 +0800

    fix bug

--- app/services/llm.py
+++ app/services/llm.py
@@ -80,7 +80,7 @@ def _generate_response(prompt: str) -> str:
             # )
             # content = response["output"]["text"]
             # return content.replace("\n", "")
-            messages=[{"role": "user", "content": prompt}]
+            messages={"role": "user", "content": prompt}
             content = normalQwen(messages=messages)
             return content.replace("\n", "")
 
